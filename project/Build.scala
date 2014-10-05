d83161a9521633d013e5b8a40e296abd56fa2420
Yuto Suzuki <yuutoo.advance@gmail.com>
2014-10-05 19:38:12 +0900

    for mysql

--- project/Build.scala
+++ project/Build.scala
@@ -11,11 +11,13 @@ object myBuild extends Build {
     id="main",
     base=file("."),
     settings = Project.defaultSettings ++ Seq(
-      scalaVersion := "2.10.3",
+      scalaVersion := "2.11.2",
       libraryDependencies ++= List(
         "com.typesafe.slick" %% "slick" % "2.1.0",
         "com.typesafe.slick" %% "slick-codegen" % "2.1.0-RC3",
         "org.slf4j" % "slf4j-nop" % "1.6.4",
+        "mysql" % "mysql-connector-java" % "5.1.26",
+        "org.scala-lang" % "scala-reflect" % scalaVersion.value,
         "com.h2database" % "h2" % "1.3.170"
       ),
       slick <<= slickCodeGenTask, // register manual sbt command
@@ -27,12 +29,19 @@ object myBuild extends Build {
   lazy val slick = TaskKey[Seq[File]]("gen-tables")
   lazy val slickCodeGenTask = (sourceManaged, dependencyClasspath in Compile, runner in Compile, streams) map { (dir, cp, r, s) =>
     val outputDir = (dir / "slick").getPath // place generated files in sbt's managed sources folder
+/*
+    val url = "jdbc:mysql://localhost/test"
+    val jdbcDriver = "com.mysql.jdbc.Driver"
+    val slickDriver = "scala.slick.driver.MySQLDriver"
+    val pkg = "demo"
+ */
     val url = "jdbc:h2:mem:test;INIT=runscript from 'src/main/sql/create.sql'" // connection info for a pre-populated throw-away, in-memory db for this demo, which is freshly initialized on every run
     val jdbcDriver = "org.h2.Driver"
     val slickDriver = "scala.slick.driver.H2Driver"
-    val pkg = "demo"
-    toError(r.run("scala.slick.codegen.SourceCodeGenerator", cp.files, Array(slickDriver, jdbcDriver, url, outputDir, pkg), s.log))
-    val fname = outputDir + "/demo/Tables.scala"
+    val pkg = "demo2"
+    toError(r.run("scala.slick.codegen.SourceCodeGenerator", cp.files,
+      Array(slickDriver, jdbcDriver, url, outputDir, pkg), s.log))
+    val fname = outputDir + "/demo2/Tables.scala"
     Seq(file(fname))
   }
 }
